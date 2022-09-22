0145156bfbe4ad2c7ca6cc351c1d8a802c839f05
Olga Yurchuk <olga.yurchuk@xored.com>
2022-09-22 11:26:20 +0700

    Initial Commit

---
+++ LuciferQuotesAlamofire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LuciferQuotesAlamofire
+//
+//  Created by Olga Yurchuk on 22.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
